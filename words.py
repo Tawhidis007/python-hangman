641ddba79178d4973a0c6b0cca2f14a56a7fbfdb
Tawhidis007 <tawhid.shahriar@yahoo.com>
2020-05-29 20:11:26 +0600

    Add files via upload
    initial commit

---
+++ words.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Hridoy
+"""
+
+import random
+
+WORDLIST = 'wordlist.txt'
+
+
+def get_random_word(min_word_length):
+    #get random word
+    num_words_processed = 0
+    curr_word = None
+    with open(WORDLIST, 'r') as f:
+        for word in f:
+            if '(' in word or ')' in word:
+                continue
+            word = word.strip().lower()
+            if len(word) < min_word_length:
+                continue
+            num_words_processed += 1
+            if random.randint(1, num_words_processed) == 1:
+                curr_word = word
+    return curr_word
